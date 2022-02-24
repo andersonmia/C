5feda2c85fedeacfe78c4b32144e9466a75d6d81
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-02-24 05:09:46 -0800

    Create traverse.c
    code to print the entered elements as over the right index

---
+++ traverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int arr[50],i,n;
+	printf("Please enter array size:");
+	scanf("%d",&n);
+	printf("Please enter array elements:");
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("Entered elements are:");
+	for(i=0;i<n;i++){
+		printf("\n%d",arr[i]);
+	}
+}
