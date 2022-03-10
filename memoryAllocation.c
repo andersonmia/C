75f30bb49e89c313d288c110e768b8f3f952fd36
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-10 11:38:57 -0800

    alloc

---
+++ memoryAllocation.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int i,n;
+	int *element;
+	printf("Enter the total number of elements: ");
+	scanf("%d",&n);
+	
+	element = (int*)calloc(n,sizeof(int));
+	
+	if(element == NULL){
+		printf("Error. Not enough memory");
+		exit(0);
+	}
+	for(i = 0; i < n; i++){
+		scanf("%d",element+i);
+	}
+	for(i = 1; i <n; i++){
+		if(*element > *(element+i)){
+			*element = *(element+i);
+		}
+	}
+	printf("Smallest element is %d",*element);
+	return 0;
+}
