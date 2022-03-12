ab60f89379865da78ceba0fcdb6c369e617d7465
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-12 11:20:07 -0800

    Create stringManipulation.c
    built in to manipulate strings

---
+++ stringManipulation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char name[] = "Mia";
+	printf("The length of thr string is: %d",strlen(name));
+char dad[] = "Anthony";
+printf("\nMy two names are: ",name.strcat(dad));
+printf("%s to uppercase is : %s",name,strupr(name));
+}
+
